434208e461ee3474590549f2a3290836b7894f48
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-06-02 11:37:06 -0300

    att

---
+++ CleanOceanic/CleanOceanic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanOceanic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
